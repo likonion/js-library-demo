999dd643e9b7cc9482dbd10b2c28e121fddadc28
vincent <likonion@outlook.com>
2017-11-24 11:46:09 +0800

    fix

--- Tab/tab.js
+++ Tab/tab.js
@@ -2,32 +2,45 @@
  * Created by likonion on 2016/4/22.
  */
 
-window.onload=function(){
+window.onload = function () {
     new TabSwitch('Tab1');
     new TabSwitch('Tab2');
 }
 
 
 function TabSwitch(id) {
+    /*元素获取*/
     var tabbox = document.getElementById(id);
-    this.tabTitle = tabbox.getElementsByTagName('ul')[0].getElementsByTagName('li');
+    //获取选项卡展示部分
     this.tabContent = tabbox.getElementsByTagName("ul")[1].getElementsByTagName('li');
-    var i = 0;
-    var _this=this;
-    for (i = 0; i < this.tabTitle.length; i++) {
+    //获取选项卡控制部分
+    this.tabTitle = tabbox.getElementsByTagName('ul')[0].getElementsByTagName('li');
+    
+    /*变量设置*/
+    //选项卡张数
+    this.count = this.tabTitle.length;
+    //当前第几张
+    this.cur = 0;
+    var _this = this;
+    for (var i = 0; i < this.count; i++) {
+        //设置索引
         this.tabTitle[i].index = i;
-        this.tabTitle[i].onclick = function(){
-            _this.tab(this);
+        //给按钮添加事件
+        this.tabTitle[i].onclick = function () {
+            _this.cur = this.index;
+            _this.switch();
         };
     }
+
 }
-TabSwitch.prototype.tab=function(oBtn) {
-    for (i = 0; i < this.tabTitle.length; i++) {
+TabSwitch.prototype.switch = function () {
+    //去掉所有
+    for (var i = 0; i < this.count; i++) {
         this.tabTitle[i].className = '';
         this.tabContent[i].style.display = 'none';
-
     }
-    oBtn.className = 'active';
-    this.tabContent[oBtn.index].style.display = 'block';
+    //显示当前
+    this.tabTitle[this.cur].className = 'active';
+    this.tabContent[this.cur].style.display = 'block';
 
 }
